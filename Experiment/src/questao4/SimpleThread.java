79e661145caba79bea5eaaf200aeea8dcd31c068
Mariana Mendes e Silva <mariana.silva@ccc.ufcg.edu.br>
2018-12-10 23:51:40 -0300

    ajuste do experimento java

--- Experiment/src/questao4/SimpleThread.java
+++ Experiment/src/questao4/SimpleThread.java
@@ -1,39 +1,35 @@
 package questao4;
 
-import java.text.NumberFormat;
-
 class SimpleThread extends Thread {
-    public SimpleThread() {
-    }
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            try {
-                sleep((int)(Math.random() * 1000));
-            } catch (InterruptedException e) {}
-        }
-    }
+	public SimpleThread() {
+	}
+
+	public void run() {
+
+		for (int i = 0; i < 10; i++) {
+			try {
+				sleep((int) (Math.random() * 1000));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
 }
 
 class Main {
-    public static void main(String[] args) {
-    	
-    	Runtime runtime = Runtime.getRuntime();
+	public static void main(String[] args) {
+		Runtime runtime = Runtime.getRuntime();
+		System.out.println("Diferenca: "+  (runtime.totalMemory() - runtime.freeMemory())/1024);
 
-    	NumberFormat format = NumberFormat.getInstance();
+		System.out.println("Memoria inicial: " + runtime.totalMemory()/1024);	
+		for (int j = 0; j < 12; j++) {
+			for (int i = 0; i < Math.pow(2, j); i++) {
+				new SimpleThread().start();
+				new SimpleThread().start();
+			}
 
-    	StringBuilder sb = new StringBuilder();
-    	long allocatedMemory = runtime.totalMemory();
-    	sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
-    	
-    	for (int i = 0; i < 10; i++) {
-        	long allocatedMemory2 = runtime.totalMemory();
-        	sb.append("allocated memory: " + format.format(allocatedMemory2 / 1024) + "<br/>");
-        	System.out.println(sb);
-            new SimpleThread().start();
-            new SimpleThread().start();
-    	}
-   
-    	
-
-    }
+			long allocatedMemory2 = runtime.totalMemory() - runtime.freeMemory();
+			System.out.println("numero de threads: " + (int) Math.pow(2, j) * 2);
+			System.out.println("memoria: " + allocatedMemory2 / 1024);
+		}
+	}
 }
